068688b7284a592cf931bba47822d2d98b4b1fd2
Paul Barrett <fastturtle4@gmail.com>
2018-10-21 23:21:56 -0400

    added comic functionality

--- node-10-http0.js
+++ node-10-http0.js
@@ -7,6 +7,8 @@
 
 const LOWERPORT = 34999;
 const UPPERPORT = 34999;
+const exec = require('child_process').exec;
+
 var acceptableURL = /^\/COMIC\/\d\d\d\d\-\d\d\-\d\d$|^\/COMIC\/CURRENT$|^\/SEARCH\/[a-zA-Z0-9]+$|^\/MYFILE\/[a-zA-Z0-9]+.html$/; 
 
 var http = require("http"),
@@ -19,19 +21,38 @@ function randomizePort(startport, endport) {
 const hostname = 'violet.cs.uky.edu';
 const port = randomizePort(LOWERPORT, UPPERPORT);
 
-function doprocess(request, response) {
+function giveComic(acceptedURL){
+	if(acceptedURL.substring(7) == "CURRENT"){
+		console.log("nice job picking that substring champ");
+		exec('wget ' + 'http://dilbert.com', function(error, stdout, stderror){
+			if (error){
+				console.error('Exec error'+error);
+				return;
+			}
+			console.log('stdout'+stdout);
+			console.log('stderror'+stderror);
+		});
+	} else {
+		console.log("oops");
+	}
+}
+
+function serveURL(request, response) {
 	var xurl = request.url;
 	if(xurl.match(acceptableURL)) {
 		response.statusCode = 200;
 		response.setHeader('Content-Type', 'text/plain');
 		response.end('Hello, World!  You requested the following URL: '+xurl+'\n');
 		console.log("VALID: Hey, the client requested the URL: ("+xurl+")");
+		giveComic(xurl);
 	} else {
 		console.log("BAD:   Hey, the client requested the URL: ("+xurl+")");
 	}
 }
 
-var server = http.createServer(doprocess);
+
+
+var server = http.createServer(serveURL);
 console.log("Server started. Listening on http://"+hostname+":"+port);
 server.listen(port,hostname);
 
