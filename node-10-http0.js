94f277fbde93ec00dd8ec2f736baa519600521f1
Paul Barrett <fastturtle4@gmail.com>
2018-10-23 14:35:20 -0400

    added exec function

--- node-10-http0.js
+++ node-10-http0.js
@@ -8,7 +8,8 @@
 const LOWERPORT = 34999;
 const UPPERPORT = 34999;
 const exec = require('child_process').exec;
-
+const hostname = 'violet.cs.uky.edu';
+const port = randomizePort(LOWERPORT, UPPERPORT);
 var acceptableURL = /^\/COMIC\/\d\d\d\d\-\d\d\-\d\d$|^\/COMIC\/CURRENT$|^\/SEARCH\/[a-zA-Z0-9]+$|^\/MYFILE\/[a-zA-Z0-9]+.html$/; 
 
 var http = require("http"),
@@ -18,50 +19,66 @@ function randomizePort(startport, endport) {
 	return Math.floor(Math.random() * (endport - startport + 1)) + startport;
 }
 
-const hostname = 'violet.cs.uky.edu';
-const port = randomizePort(LOWERPORT, UPPERPORT);
+function giveFile(acceptedURL){
+	var requestedFile = acceptedURL.substring(8);
+}
 
-function giveComic(acceptedURL){
-	if(acceptedURL.substring(7) == "CURRENT"){
-		console.log("nice job picking that substring champ");
+function callCurlExec(reqURL, isCurrent, response){
+	if(isCurrent){
 		exec('curl ' + '"http://dilbert.com"', {env: {'PATH': '/usr/bin'}}, function(error, stdout, stderror){
 			if (error){
 				console.error('Exec error'+error);
 				return;
 			}
-			console.log('stdout'+stdout);
-			console.log('stderror'+stderror);
+			//console.log('stdout'+stdout);
+			//console.log('stderror'+stderror);
 		});
 	} else {
-		console.log("oops");
+		exec('curl ' + '"http://dilbert.com/strip/' + reqURL + '"', {env: {'PATH': '/usr/bin'}}, function(error, stdout, stderror){
+			if (error){
+				console.error('Exec error'+error);
+				return;
+			}
+			console.log("above");
+			response.write(stdout);
+			console.log("middle");
+			//response.end();
+			console.log("below");
+			//setTimeout(function() { console.log("yo");response.end(); }, 10000);
+		});
 	}
+}
+
+function giveComic(acceptedURL, response){
+	var requestedURL = acceptedURL.substring(7);
+	var isCurrentURL;
+	console.log("top");
+	if(requestedURL == "CURRENT"){
+		isCurrentURL = true;
+		
+	} else if (requestedURL.match(/^\d\d\d\d\-\d\d\-\d\d$/)){
+		isCurrentURL = false;
+			
+	}
+	callCurlExec(requestedURL, isCurrentURL, response);
+	//response.end();
+	console.log("far bottom");
 }
 
 function serveURL(request, response) {
 	var xurl = request.url;
+	response.statusCode = 200;
+	response.setHeader('Content-Type', 'text/html');
+	response.write('Hello, World! You requested the following URL: '+xurl+'\n');
 	if(xurl.match(acceptableURL)) {
-		response.statusCode = 200;
-		response.setHeader('Content-Type', 'text/plain');
-		response.end('Hello, World!  You requested the following URL: '+xurl+'\n');
 		console.log("VALID: Hey, the client requested the URL: ("+xurl+")");
-		giveComic(xurl);
+		giveComic(xurl, response);
 	} else {
 		console.log("BAD:   Hey, the client requested the URL: ("+xurl+")");
+		response.end();
 	}
 }
-
-
 
 var server = http.createServer(serveURL);
 console.log("Server started. Listening on http://"+hostname+":"+port);
 server.listen(port,hostname);
-
-
-
-
-
-
-
-
-
-
